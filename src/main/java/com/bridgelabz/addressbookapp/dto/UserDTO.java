d55dc2ace570ba8d433faf4027d3c15e4bf5e1aa
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-10-17 09:50:02 +0530

    Create DTO class

---
+++ src/main/java/com/bridgelabz/addressbookapp/dto/UserDTO.java
@@ -0,0 +1,68 @@
+package com.bridgelabz.addressbookapp.dto;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String contac;
+    private String city;
+    private String state;
+
+    public UserDTO(String firstName, String lastName, String contac, String city, String state) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contac = contac;
+        this.city = city;
+        this.state = state;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContac() {
+        return contac;
+    }
+
+    public void setContac(String contac) {
+        this.contac = contac;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", contac='" + contac + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
